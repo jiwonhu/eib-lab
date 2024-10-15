5bc92ca0b64264a27e6b11aac2f1312fb470a5f6
Jiwon Hu <jiwon.hu@suse.com>
2024-10-15 09:01:58 +0900

    Add 2 examples for Mac

---
+++ mac/1-rancher-2nodes/custom/scripts/99-register.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -euo pipefail
+
+# Registration https://www.suse.com/support/kb/doc/?id=000018564
+#if ! which SUSEConnect > /dev/null 2>&1; then
+#	zypper --non-interactive install suseconnect-ng
+#fi
+#SUSEConnect --email "alberto.morgante@suse.com" --url "https://scc.suse.com" --regcode "INTERNAL-USE-ONLY-f024-d052"
